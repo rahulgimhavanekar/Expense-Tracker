7ca12290e3f044d8d3c200f50019af101e217bd4
Rahul Gimhavanekar <rahulgimhavanekar@gmail.com>
2022-04-10 20:34:10 +0530

    feat: Added Routing to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import { Fragment } from "react";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import DashboardPage from "./pages/DashboardPage";
+import ExpensesPage from "./pages/ExpensesPage";
+import HomePage from "./pages/HomePage";
+import IncomePage from "./pages/IncomePage";
 
 function App() {
   return (
     <Fragment>
-      <Layout></Layout>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/expenses" element={<ExpensesPage />} />
+          <Route path="/income" element={<IncomePage />} />
+        </Routes>
+      </Layout>
     </Fragment>
   );
 }
--- client/src/components/Layout/Sidebar.js
+++ client/src/components/Layout/Sidebar.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import dashboardIcon from "../../assets/dashboard_icon.svg";
 import expensesIcon from "../../assets/expenses_icon.svg";
+import incomeIcon from "../../assets/income_icon.svg";
 import classes from "./Sidebar.module.css";
 
 const Sidebar = () => {
@@ -8,16 +10,22 @@ const Sidebar = () => {
     <div className={classes.sidebar}>
       <ul className={classes.item_list}>
         <li>
-          <div className={classes.item}>
+          <NavLink className={classes.item} to="/dashboard">
             <img src={dashboardIcon} alt="dashboard" />
             Dashboard
-          </div>
+          </NavLink>
         </li>
         <li>
-          <div className={classes.item}>
+          <NavLink className={classes.item} to="/expenses">
             <img src={expensesIcon} alt="expenses" />
             Expenses
-          </div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={classes.item} to={"/income"}>
+            <img src={incomeIcon} alt="income" />
+            Income
+          </NavLink>
         </li>
       </ul>
     </div>
